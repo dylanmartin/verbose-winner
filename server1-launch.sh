fa1ce3691ffdbb9ac755215406c69b140ba1560e
Dylan Martin <dylanrossmartin@gmail.com>
2024-01-17 13:07:16 -0500

    job deployed and completed

--- server1-launch.sh
+++ server1-launch.sh
@@ -1,8 +1,8 @@
 docker run --rm --gpus all \
     --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
-    --name server1 \
+    --name host.docker.internal \
     -p 8002:8002 -p 8003:8003 \
-    -v //c/development/verbose-winner/my-workspace/workspace/example_project/prod_00/server1:/provision \
+    -v //c/development/verbose-winner/my-workspace/workspace/example_project/prod_00/host.docker.internal:/provision \
     -w //my-workspace \
     nvflare-pt:latest \
     //bin/bash -c "../provision/startup/start.sh; tail -f /dev/null"
